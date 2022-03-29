a4d4580990d87f3414f730ffcfc92ea9ac0a3c3b
Zach Davis <zacharydavis92@gmail.com>
2022-03-29 11:26:41 -0500

    add basic post routes

--- controllers/helpPost-routes.js
+++ controllers/helpPost-routes.js
@@ -4,7 +4,27 @@ router.get('/', (req, res) => {
     res.json({ message: 'working'})
     .catch(err => {
         res.status(500).json(err)
-    })
-})
+    });
+});
+
+router.get('/:tag', (req, res) => {
+
+});
+
+router.get('/:user', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/update', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
 
 module.exports = router;
--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
     res.json({ message: 'working'})
     .catch(err => {
         res.status(500).json(err)
