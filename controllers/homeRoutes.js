12e55db952ac5cbc975909bbca605a4003603fb4
Zach Davis <zacharydavis92@gmail.com>
2022-04-02 16:54:45 -0500

    save to switch

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,34 +3,18 @@ const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
     res.render('homepage')
-    // .catch(err => {
-    //     console.log(err)
-    //     res.status(500).json(err);
-    // })
 })
 
 router.get('/login', withAuth, (req, res) => {
     res.render('login')
-    .catch(err => {
-        console.log(err)
-        res.status(500).json(err);
-    })
 });
 
 router.get('/signUp', (req, res) => {
     res.render('signUp')
-    .catch(err => {
-        console.log(err)
-        res.status(500).json(err);
-    })
 });
 
 router.get('/about', (req, res) => {
     res.render('about')
-    .catch(err => {
-        console.log(err)
-        res.status(500).json(err);
-    })
 });
 
 module.exports = router;
