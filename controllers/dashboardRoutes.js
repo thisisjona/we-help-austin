7f2bb326b3222753e4b5060042fddc8cb7aab5bb
Zach Davis <zacharydavis92@gmail.com>
2022-04-04 17:50:17 -0500

    added signup function

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        attributes: [
+            'title',
+            'username',
+            'body',
+            'deadline',
+            'tag',
+            'requirements'
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const helpPost = require('./helpPost-routes');
 const userRoutes = require('./userRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/', homeRoutes);
 router.use('/helpPost', helpPost);
 router.use('/user', userRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
