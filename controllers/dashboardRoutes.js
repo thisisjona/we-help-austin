8abd7d2df3b5ba395c78af9f0b09d8e99fa49bcf
Zach Davis <zacharydavis92@gmail.com>
2022-04-05 09:38:50 -0500

    fixed profile route

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -6,6 +6,7 @@ const withAuth = require('../utils/auth');
 router.get('/', withAuth, (req, res) => {
     Post.findAll({
         attributes: [
+            'id',
             'title',
             'username',
             'body',
