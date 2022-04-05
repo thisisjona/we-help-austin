d04bf79f8e47d601998f195ea420e5de7292dda1
Zach Davis <zacharydavis92@gmail.com>
2022-04-05 16:21:16 -0500

    fixed update post button

--- public/javascript/postPage.js
+++ public/javascript/postPage.js
@@ -1,19 +1,22 @@
 async function updateButton() {
-    // const updateBtn = document.querySelector('#update-btn');
-    // const postUser = document.querySelector('#postUser');
-    // const id = window.location.toString().split('/')[
-    //     window.location.toString().split('/').length -1];
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length -1];
 
-    // const response = await fetch(`/helpPost/${id}`, {
-    //     method: 'get',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    // }).then(response => response.json())
-    // .then(data => {
-    //     console.log(data)
-    // })
-    document.location.replace('/postUpdate')
-  }
+    const response = await fetch(`/helpPost/checkUser`, {
+        method: 'post',
+        body: JSON.stringify({id}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.check) {
+        document.location.assign(`/helppost/update/${id}`)
+      } else {
+        alert('Not your post to change!')
+      }
+    });
+}
 
-  document.querySelector('#update-btn').addEventListener('click', updateButton);
+document.querySelector('#update-btn').addEventListener('click', updateButton);
